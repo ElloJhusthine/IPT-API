a3d357d5afd27f35fa088464aab1ee7e8342d709
ElloJhusthine <80324094+ElloJhusthine@users.noreply.github.com>
2025-02-20 10:09:08 +0800

    Add files via upload

---
+++ create-of-project/restapiproject/post/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
+
+
+urlpatterns = [
+    path('post/', PostListCreateAPIView.as_view(), name='list-create-post'),
+    path('post/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='retrieve-update-destroy-post'),
+]
